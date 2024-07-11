7330fe7d61feb928e0c2d1e0dc0717c3a14a2ba2
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-07-11 13:26:59 -0300

    Extract URL path

--- app/main.py
+++ app/main.py
@@ -7,14 +7,15 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 4221)) #, reuse_port=True)
     connection, address = server_socket.accept() # wait for client
     
     # Read data from a connection
     data = connection.recv(1024)
 
     # Extract URL path then respond accordingly
-    url_path = data.split()[1]
+    url_path = data.decode("utf-8").split()[1]
+    
     if url_path == "/":
         # make the server respond back with HTTP/1.1 200 OK\r\n\r\n
         connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n') # it sends back data in binary format as well
