17b255eb50825393f8318752145fba430293e461
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-07-11 13:04:43 -0300

    Extract URL path

--- app/main.py
+++ app/main.py
@@ -10,8 +10,13 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
     
-    # make the server respond back with HTTP/1.1 200 OK\r\n\r\n 
-    connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+    # Extract URL path then respond accordingly
+    url_path = connection.recv(1024).split()[1]
+    if url_path is "/":
+        # make the server respond back with HTTP/1.1 200 OK\r\n\r\n
+        connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n') # it sends back data in binary format as well
+    else:
+        connection.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n')
 
 if __name__ == "__main__":
     main()
