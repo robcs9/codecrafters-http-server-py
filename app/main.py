01e9e3b3e0de2944c448c10a3a3df7583b0ed502
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-07-10 21:46:43 -0300

    Respond with 200

--- app/main.py
+++ app/main.py
@@ -8,8 +8,10 @@ def main():
 
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    connection, address = server_socket.accept() # wait for client
+    
+    # make the server respond back with HTTP/1.1 200 OK\r\n\r\n 
+    connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
 
 if __name__ == "__main__":
     main()
