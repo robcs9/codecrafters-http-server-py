0775908fe09413d8670f3cce5195d74e5b7e6978
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-07-13 22:17:09 -0300

    Respond with body

--- app/main.py
+++ app/main.py
@@ -17,17 +17,18 @@ def main():
     data_str = str(data)
     lines = data_str.split()[1] 
     path = lines.split('/')
-    target_path = path.pop()
+    target_path = path[-1]
     
     # Assign content length for the response
     path_size = len(target_path)
     
     # Send back appropriate response
+    # ['', 'echo', 'x'] => 202 'x', ['', ''] => 202, ['', 'y'] => 404
     response = f'HTTP/1.1 404 Not Found\r\n\r\n'
-
-    print(len(path))
-    if path[-1] == "echo" or len(path) < 2:
+    if len(path) > 1 and path[1] == 'echo':
         response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {path_size}\r\n\r\n{target_path}'
+    if len(path) == 2 and path[1] == '':
+        response = f'HTTP/1.1 200 OK\r\n\r\n'
     
 
     connection.send(str.encode(response))
