07508677ac1cff62f210ee8de5ba3c8da14f5405
robcs9 <83130807+robcs9@users.noreply.github.com>
2024-07-26 22:25:54 -0300

    Gzip compression

--- app/main.py
+++ app/main.py
@@ -89,10 +89,10 @@ def handle_response(connection):
         compressed_body = gzip.compress(bytes(body, "utf-8"))
         compressed_zbody = zlib.compress(bytes(body, "utf-8"))
         #print(compressed_zbody.decode())
-        print(compressed_body.decode())
+        print(compressed_body.decode('utf-8'))
         #content_length_index = response.find()
         #response = f'{response[:header_beginning]}Content-Encoding: gzip\r\n{response[header_beginning:]}'
-        response = f'HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(compressed_zbody)}\r\n\r\n{compressed_zbody.decode()}'
+        response = f'HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: {len(compressed_zbody)}\r\n\r\n{compressed_zbody}'
         #response = (
         #    b'HTTP/1.1 200 OK\r\nContent-Encoding: gzip\r\nContent-Type: text/plain\r\nContent-Length: '
         #    + len(compressed_zbody)
